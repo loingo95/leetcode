a0a7be4bc3d8a7fb0c2e1c52465ed6a16216a165
Leonn <leonn@cinnamon.is>
2021-07-02 22:12:34 +0700

    Runtime: 52 ms(70.19%), Memory Usage: 15.5 MB(27.90%)

--- third-maximum-number/third-maximum-number.py
+++ third-maximum-number/third-maximum-number.py
@@ -1,8 +1,27 @@
 class Solution:
     def thirdMax(self, nums: List[int]) -> int:
+        ## Naive
+        # unique_nums = list(set(nums))
+        # sorted_nums = sorted(unique_nums)
+        # if len(sorted_nums) < 3:
+        #     return sorted_nums[-1]
+        # return sorted_nums[-3]
+
         unique_nums = list(set(nums))
-        sorted_nums = sorted(unique_nums)
-        if len(sorted_nums) < 3:
-            return sorted_nums[-1]
-        return sorted_nums[-3]
-        
+
+        if len(unique_nums) < 3:
+            return max(unique_nums)
+
+        first = second = third = min(unique_nums)
+        for num in unique_nums:
+            if num >= first:
+                third = second
+                second = first
+                first = num
+            elif num >= second:
+                third = second
+                second = num
+            elif num > third:
+                third = num
+        print(first, second, third)
+        return third
