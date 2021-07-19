ca45c4edb8493b0dd8fa1bf8457c185e3cf8e8cf
Ngo Thanh Loi (Leonn) <leonn@cinnamon.is>
2021-07-19 21:53:23 +0700

    Time: 52 ms (63.04%), Space: 18.2 MB (28.07%) - LeetHub

--- kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
+++ kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
@@ -6,20 +6,29 @@
 #         self.right = right
 class Solution:
     def kthSmallest(self, root: TreeNode, k: int) -> int:
-        self.smallests = [float("inf")]*k
-        self.find_with_inorder_traversal(root)
+        self.k_smallests = [float("inf")]*k
+        self.find_with_inorder_traversal(root, lower_limit=float("-inf"))
             
-        return self.smallests[-1]
+        return self.get_kth_smallest()
+    
+    def get_kth_smallest(self):
+        return self.k_smallests[-1]
     
     def update_smallest(self, node):
-        if node and node.val < self.smallests[-1]:
-            self.smallests[-1] = node.val
-            self.smallests = sorted(self.smallests)
+        if node and node.val < self.k_smallests[-1]:
+            self.k_smallests[-1] = node.val
+            self.k_smallests = sorted(self.k_smallests)
 
-    def find_with_inorder_traversal(self, node):
+    def find_with_inorder_traversal(self, node, lower_limit):
+#         If lower limit of a tree is already >= current kth smallest,
+#         then we no longer need to iter through this tree
+        if lower_limit >= self.get_kth_smallest():
+            return
+            
         if node is None:
             return
+        
         if node:
-            self.find_with_inorder_traversal(node.left)
+            self.find_with_inorder_traversal(node.left, lower_limit)
             self.update_smallest(node)
-            self.find_with_inorder_traversal(node.right)
+            self.find_with_inorder_traversal(node.right, node.val)
