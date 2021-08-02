c4378e6b62d9efc7f3da9c7038fdcedf9955310a
Ngo Thanh Loi (Leonn) <leonn@cinnamon.is>
2021-08-02 21:54:48 +0700

    Time: 28 ms (86.24%), Space: 14 MB (92.55%) - LeetHub

--- pascals-triangle-ii/pascals-triangle-ii.py
+++ pascals-triangle-ii/pascals-triangle-ii.py
@@ -1,20 +1,13 @@
 class Solution:
-    pascal_triangle = []
     def getRow(self, rowIndex: int) -> List[int]:
         if rowIndex == 0:
-            if len(self.pascal_triangle) == 0:
-                self.pascal_triangle.append([1])
-            return self.pascal_triangle[0]
+            return [1]
         
-        self.getRow(rowIndex-1)
+        prev = self.getRow(rowIndex - 1)
         
-        row_val = [1]*(rowIndex+1)
+        row_val = [1] * (rowIndex + 1)
         for col_idx in range(1, rowIndex):
-            row_val[col_idx] = self.pascal_triangle[rowIndex-1][col_idx-1]+ \
-            self.pascal_triangle[rowIndex-1][col_idx]
+            row_val[col_idx] = prev[col_idx - 1]+ prev[col_idx]
         
-        if len(self.pascal_triangle) == rowIndex:
-            self.pascal_triangle.append(row_val)
-
         return row_val
         
