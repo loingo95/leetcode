0543f421a5511ee2da6a235df9f84a4e7aba3782
Ngo Thanh Loi (Leonn) <leonn@cinnamon.is>
2021-08-09 21:08:12 +0700

    Time: 92 ms (69.88%), Space: 52.4 MB (5.02%) - LeetHub

--- reorder-list/reorder-list.py
+++ reorder-list/reorder-list.py
@@ -13,17 +13,12 @@ class Solution:
             if node.next is None:
                 return node
             
-            # print(node.val)
             tail = recur_reorder(node.next)
             
             if not tail or \
                 self.current_node == tail or \
                 self.current_node.next == tail:
                 return
-
-            
-            # print(self.current_node.val, node.val, tail.val)
-            
             
             origin_next = self.current_node.next
             self.current_node.next = tail
@@ -33,17 +28,6 @@ class Solution:
             node.next = None
             return node
         
-            
-            
-        self.head = head
         self.current_node = head
         recur_reorder(head)
         
-        
-        '''
-        origin_next = node.next
-        tail = recur_reorder(node.next)
-        node.next = get_tail(node)
-        node.next.next = origin_next
-        
-        '''
