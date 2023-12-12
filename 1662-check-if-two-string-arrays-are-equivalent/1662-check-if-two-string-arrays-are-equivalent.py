41149f7825555c476eae2a34686a6cbdc6698d52
Ngo Thanh Loi (leonncinnamon) <leonn@cinnamon.is>
2023-12-13 00:28:15 +0700

    Time: 40 ms (58.66%), Space: 16.4 MB (13.58%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.py
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.py
@@ -1,5 +1,22 @@
 class Solution:
     def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
+        '''
+        O(1) space solution
+        class Solution:
+            def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
+                def getChars(word):
+                    for s in word:
+                        for c in s:
+                            yield c
+                    yield
+
+                return all(c1 == c2 for c1, c2 in zip(getChars(word1), getChars(word2)))    
+        '''
+        '''
+        call K = maxinum length of a char in word
+        time: O(N*K)
+        space: O(N*K)
+        '''
         word1_str = "".join(word1)
         word2_str = "".join(word2)
 
