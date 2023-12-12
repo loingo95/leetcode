1ba5d0232dcb7df3dd2418bad4399a7034e8c16c
Ngo Thanh Loi (leonncinnamon) <leonn@cinnamon.is>
2023-12-13 00:54:00 +0700

    Time: 372 ms (5.02%), Space: 16.8 MB (97.69%) - LeetHub

---
+++ 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.py
@@ -0,0 +1,38 @@
+import copy
+
+
+class Solution:
+    def countCharacters(self, words: List[str], chars: str) -> int:
+        # turn chars into hash map for faster query time, key=char, value=nrof chars
+        chars_map = {}
+        for char in chars:
+            if char not in chars_map:
+                chars_map[char] = 1
+            else:
+                chars_map[char] += 1
+
+        good_strings_len = 0
+
+        for word in words:
+            # chars_map reset for each word
+            break_flag = False
+            word_chars_map = copy.deepcopy(chars_map)
+            for idx, char in enumerate(word):
+                if char in word_chars_map:
+                    # get one char
+                    word_chars_map[char] -= 1
+                    # if there is no char left, remove it
+                    if word_chars_map[char] == 0:
+                        word_chars_map.pop(char)
+                else:
+                    break_flag = True
+                    break
+            # idx reach word end mean that all char in word is formed
+            # idx start at 0, so needed to add one for lenght
+            # in case that len(word)==1, even when break, good_strings_len still got += 1p,
+            # fix this by detect break event with break_flag
+            if not break_flag:
+                good_strings_len += len(word)
+
+            
+        return good_strings_len
