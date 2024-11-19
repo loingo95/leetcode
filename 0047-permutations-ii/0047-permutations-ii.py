77eb846a875e16ec14f43044f0fe1cee7c1d3d39
Ngô Thanh Lợi (leonncinnamon) <leonn@cinnamon.is>
2024-11-20 00:05:58 +0700

    Time: 11 ms (55.15%), Space: 12.2 MB (15.41%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.py
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -9,29 +9,49 @@ class Solution(object):
         # It mean istead of do nums, we do list(range(len(nums))), in final step do converting back to nums
         # => This will not generating unique permutations, doing a final check to filter out wold do
         # But I don't think it's mean to be for this problem
+
+#         # First submitsion
+#         permutations = []
+#         nums_len = len(nums)
+#         nums_indexes = range(nums_len)
         
+#         # sort first for easily check duplicate
+#         # nums.sort()
+        
+#         def backtracking(permutation):
+#             if len(permutation) == nums_len:
+#                 nums_permutation = [nums[i] for i in permutation]
+#                 if nums_permutation not in permutations:
+#                     permutations.append(nums_permutation)
+#                 return
+                
+#             for idx in nums_indexes:
+#                 # nums now contain duplicated,
+#                 # we can not simple check if num in permutation anymore
+#                 # how about pop out the num so that it could not be add another time
+#                 if idx not in permutation:
+#                     permutation.append(idx)
+#                     backtracking(permutation)
+#                     permutation.pop()
+                    
+#         backtracking([])
+
+        from collections import Counter
+    
         permutations = []
-        nums_len = len(nums)
-        nums_indexes = range(nums_len)
-        
-        # sort first for easily check duplicate
-        # nums.sort()
+        counter = Counter(nums)
         
         def backtracking(permutation):
-            if len(permutation) == nums_len:
-                nums_permutation = [nums[i] for i in permutation]
-                if nums_permutation not in permutations:
-                    permutations.append(nums_permutation)
+            if len(permutation) == len(nums):
+                permutations.append(list(permutation))
                 return
+            
+            for num in counter:
+                if counter[num] > 0:
+                    counter[num] -= 1
+                    backtracking(permutation + [num])
+                    counter[num] += 1
                 
-            for idx in nums_indexes:
-                # nums now contain duplicated,
-                # we can not simple check if num in permutation anymore
-                # how about pop out the num so that it could not be add another time
-                if idx not in permutation:
-                    permutation.append(idx)
-                    backtracking(permutation)
-                    permutation.pop()
-                    
         backtracking([])
+        
         return permutations
